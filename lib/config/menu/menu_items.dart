9c2ba1f0baed4242718fcceda691c1b3166e8ece
Pablo Molina <pablomolina@PabloMolinaMBP.gateway>
2023-07-19 14:20:56 +1000

    menu items added

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem(
+      {required this.title,
+      required this.subTitle,
+      required this.link,
+      required this.icon});
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+      title: 'Buttons',
+      subTitle: 'List of Flutter' 's buttons',
+      link: '/buttons',
+      icon: Icons.smart_button_outlined),
+  MenuItem(
+      title: 'Cards',
+      subTitle: 'Fancy Card',
+      link: '/card',
+      icon: Icons.credit_card)
+];
