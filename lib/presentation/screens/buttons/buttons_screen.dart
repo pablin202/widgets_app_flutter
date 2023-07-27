7ba2c1617e59183af8b9066b358336aed2106250
Pablo Molina <pablomolina@Pablo-Molina-MBP.local>
2023-07-27 15:23:59 +1000

    buttons added

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -5,6 +5,70 @@ class ButtonsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buttons'),
+      ),
+      body: const _ButtonsView(),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Wrap(
+        spacing: 10,
+        children: [
+          ElevatedButton(onPressed: () => {}, child: const Text('Elevated')),
+          const ElevatedButton(
+              onPressed: null, child: Text('Elevated Disabled')),
+          FilledButton(onPressed: () => {}, child: const Text('Filled')),
+          FilledButton.tonal(
+              onPressed: () => {}, child: const Text('Filled Tonal')),
+          OutlinedButton(onPressed: () => {}, child: const Text('Outlined')),
+          TextButton(onPressed: () => {}, child: const Text('Text Button')),
+          // Y sus variaciones con íconos
+          ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.add_a_photo_outlined),
+              label: const Text('Photo')),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.app_registration_outlined)),
+          const CustomButton()
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Custom Button',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widget_app/presentation/screens/cards/cards_screen.dart';
+import 'package:widget_app/presentation/screens/home/home_screen.dart';
+
 export 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widget_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widget_app/presentation/screens/home/home_screen.dart';
+
+sealed class Destination {
+  final Widget widget;
+  final String path;
+  final String name;
+
+  Destination(this.widget, this.path, this.name);
+}
+
+class Home extends Destination {
+  Home(super.widget, super.path, super.name);
+}
+
+class Buttons extends Destination {
+  Buttons(super.widget, super.path, super.name);
+}
+
+class Cards extends Destination {
+  Cards(super.widget, super.path, super.name);
+}
+
+class Destinations {
+  late final Home home = Home(const HomeScreen(), '/', 'home');
+  late final Buttons buttons =
+      Buttons(const ButtonsScreen(), 'buttons', 'buttons');
+  late final Cards cards = Cards(const CardsScreen(), 'cards', 'cards');
+}
